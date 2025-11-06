75aa13334e42673205588dbe3757184f536060bb
Meet Agrawal <maagrawal2112@gmail.com>
2025-11-06 23:35:36 +0530

    Refactor createExpense function to include validation and response handling

--- controllers/expense.controller.js
+++ controllers/expense.controller.js
@@ -2,9 +2,30 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Expense } from "../models/expense.model.js";
+import e from "express";
 
 
 const createExpense = asyncHandler(async (req, res) => {
-    const userId=req.user._id;
-    let
-})
+    const userId = req.user._id; // from verifyJwt middleware
+    const { title, amount, category, date, notes, paymentMethod } = req.body;
+  
+    // Validate required fields
+    if (!title?.trim() || !amount || !category?.trim() || !date) {
+      throw new ApiError(400, "Title, amount, category, and date are required");
+    }
+
+    const expense=Expense.create({
+        userId,
+        amount,
+        title:title.trim(),
+        category:category.trim(),
+        date: new Date(date),
+        notes:notes?.trim(),
+        paymentMethod:paymentMethod || "cash"
+
+    });
+    res.status(201).json(new ApiResponse(201,expense,"Expense created successfully"));
+
+});
+
+export {createExpense};
